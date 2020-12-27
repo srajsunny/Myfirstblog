42d9fb8612a591bfd605561c856e591410ec3fbf
Sunny <rajsunny2597@gmail.com>
2020-12-27 19:26:18 +0530

    calcadd

--- calc/views.py
+++ calc/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request,'home.html',{'name':'Sunny'}) 
+    return render(request,"home.html",{"name": "Sunny"})
+
+
+
+def add(request):
+
+    a=int(request.POST['num1'])
+    
+    b=int(request.POST['num2'])
+    sum=a+b
+    return render(request, "result.html",{"result": sum})
